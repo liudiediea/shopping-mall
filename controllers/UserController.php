5b269c7263f8c5295e865f2d21d1d4033bf0aabb
刘佳怡 <liujiayi@czxy.com>
2018-09-25 16:59:36 +0800

    初始化项目

---
+++ controllers/UserController.php
@@ -0,0 +1,12 @@
+<?php
+namespace controllers;
+class UserController{
+
+    public function hello(){
+        echo "hello hello";
+    }
+
+    public function user(){
+        echo "hello user";
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,30 @@
+<?php
+
+    define('ROOT',__DIR__.'/../');
+
+    //类的自动加载
+     function auload($class){
+
+        $path = str_replace('\\','/',$class).'.php';
+        // var_dump(ROOT.$path);
+        require(ROOT.$path);
+        
+      
+
+    }
+    spl_autoload_register('auload');
+
+    //解析路由
+    $controller = '\controllers\IndexController';
+    $action = 'index';
+
+    if(isset($_SERVER['PATH_INFO'])){
+
+        $route = explode('/', $_SERVER['PATH_INFO']);
+        $controller = '\controllers\\'.ucfirst($route[1]).'Controller';
+        $action = $route[2];
+    }
+
+    $c = new $controller;
+    $c->$action();
+    
